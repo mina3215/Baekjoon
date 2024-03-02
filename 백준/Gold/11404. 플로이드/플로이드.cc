f2fb834db62aa6c047b822c4523bd5221ab4ac0b
박민아 <80940032+mina3215@users.noreply.github.com>
2024-03-02 20:37:14 +0900

    [Gold IV] Title: 플로이드, Time: 88 ms, Memory: 2144 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.cc
+++ 백준/Gold/11404. 플로이드/플로이드.cc
@@ -1,78 +1,49 @@
 #include<iostream>
-#include<vector>
-#include<queue>
 #include<algorithm>
 
 using namespace std;
 
-int N,M;
-
-long long cost[102][102];
-typedef pair<int, int> pii;
-
-void Dijkstra(int n, vector<vector<pii>>&b_stop) {
-	long long  visited[102];
-	priority_queue<pii> pq;
-
-	fill(visited, visited + 102, 2e9);
-
-	int start = n;
-
-	pq.push({ 0, n });
-
-	while (!pq.empty()) {
-		//비용 : 양의 값으로 꺼내옴
-		int v = -pq.top().first;
-		//현재 도시
-		int b = pq.top().second;
-		pq.pop();
-
-		if (visited[b] !=2e9) continue;
-		
-		visited[b] = v;
-
-		for (int i = 0; i < b_stop[b].size(); i++) {
-			
-			//갈 수 있는 도시
-			int l = b_stop[b][i].second;
-			//지금까지의 비용에 더한다
-			int c = visited[b] + b_stop[b][i].first;
-			
-			if (visited[l] != 2e9) continue;
-			
-			pq.push({ -c, l });
-		}
-	}
-
-	for (int i = 1; i <= N; i++) {
-		cost[n][i] = visited[i];
-		if (visited[i] == 2e9)cost[n][i] = 0;
-	}
-}
-
+int cost[102][102];
+long long dist[102][102];
+int N, M;
 
 int main() {
-	ios::sync_with_stdio(false);
+
+	fill(&cost[0][0], &cost[0][0] + 102 * 102, 2e9);
 	cin >> N;
 	cin >> M;
 
-	vector<vector<pii>> b_stop(N+1);
-	
-	for (int j = 0; j < M; j++) {
+	for (int i = 0; i < M; i++) {
 		int a, b, c;
 		cin >> a >> b >> c;
-		b_stop[a].push_back({ c, b });
-	}
-
-	for (int i = 1; i <= N; i++) {
-		Dijkstra(i,b_stop);
+		//a에서 b까지 가는 비용 c를 넣음
+		cost[a][b] = min(c,cost[a][b]);
 	}
 
 	for (int i = 1; i <= N; i++) {
 		for (int j = 1; j <= N; j++) {
-			cout <<cost[i][j]<<' ';
+			dist[i][j] = cost[i][j];
+			if (i == j) dist[i][j] = 0;
+		}
+	}
+
+	for (int k = 1; k <= N; k++) {
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= N; j++) {
+				dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+			}
+		}
+	}
+
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= N; j++) {
+			if (dist[i][j] >= 2e9) dist[i][j] = 0;
+			cout << dist[i][j] << ' ';
 		}
 		cout << '\n';
 	}
 
+
+	
+	return 0;
 }
