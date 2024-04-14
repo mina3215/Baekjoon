3c8bca862ca1d13f3efc66c1a3fed4f8b85eff0d
박민아 <80940032+mina3215@users.noreply.github.com>
2024-04-14 21:36:05 +0900

    [Gold III] Title: 파일 합치기, Time: 192 ms, Memory: 3980 KB -BaekjoonHub

--- 백준/Gold/11066. 파일 합치기/파일 합치기.cc
+++ 백준/Gold/11066. 파일 합치기/파일 합치기.cc
@@ -14,15 +14,22 @@ int main() {
 		int N;
 		cin >> N;
 		fill(&ans[0][0], &ans[0][0] + 501 * 501, 2e9);
-		fill(&DP[0][0], &DP[0][0] + 501 * 501, 2e9);
+		fill(&DP[0][0], &DP[0][0] + 501 * 501, 0);
 		for (int i = 0; i < N; i++) {
 			cin >> DP[i][i];
 			ans[i][i] = 0;
 		}
+
+		for (int s = 0; s < N-1; s++) {
+			for (int e = s + 1; e < N ;e++) {
+				for (int i = s; i <= e;i++)
+					DP[s][e] += DP[i][i];
+			}
+		}
+
 		for (int l = 1; l < N; l++) {
 			for (int s = 0; s < N - l; s++) {
 				for (int m = 0; m < l; m++) {
-					DP[s][s + l] = min(DP[s][s + l], DP[s][s + m] + DP[s + m + 1][s + l]);
 					ans[s][s + l] = min(ans[s][s + l], ans[s][s + m] + ans[s + m + 1][s + l]+DP[s][s+l]);
 				}
 			}
