bd09f245e189d38800e990a0c81a12b927f9520b
박민아 <80940032+mina3215@users.noreply.github.com>
2024-07-15 22:37:28 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 604 ms, Memory: 8176 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int main() {
+	int N;
+	cin >> N;
+	vector<int> v;
+	for (int i = 0; i < N; i++) {
+		int n;
+		cin >> n;
+		v.push_back(n);
+	}
+	sort(v.begin(), v.end());
+	for (auto i : v)
+		cout << i << '\n';
+}
